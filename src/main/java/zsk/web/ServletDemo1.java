584bf420cdb2f90cae143da35c6c9eeaba55e856
“zsk” <3057423823@qq.com>
2022-10-25 23:16:46 +0800

    demo

--- src/main/java/zsk/web/ServletDemo1.java
+++ src/main/java/zsk/web/ServletDemo1.java
@@ -28,7 +28,7 @@ public class ServletDemo1 extends HttpServlet {
         //request.getRequestDispatcher("/el-demo.jsp").forward(request,response);
         //request.getRequestDispatcher("/jstl-if.jsp").forward(request,response);
         request.getRequestDispatcher("/jstl-foreach.jsp").forward(request,response);
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("_______________________________________________");
     }
 
     @Override
